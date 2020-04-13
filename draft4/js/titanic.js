32b4b137cd1b0c71f91327ea9e4a53f0bcbc82dc
lilyannyang168 <36946555+lilyannyang168@users.noreply.github.com>
2020-04-12 21:51:00 -0500

    Add files via upload

---
+++ draft4/js/titanic.js
@@ -0,0 +1,322 @@
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+    	$('#image1').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '113%'},400);
+
+                    
+            }
+            
+        }); 
+
+               $('#text1').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'top': '145%'},500);
+
+                    
+            }
+            
+        }); 
+
+
+         $('#subtitle1').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 200;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '242%'},400);
+
+                    
+            }
+            
+        }); 
+
+         $('#titanic').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '310%'},400);
+
+                    
+            }
+            
+        }); 
+
+              $('#text2').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'top': '353%'},500);
+
+                    
+            }
+            
+        }); 
+
+              $('#poster').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '415%'},400);
+
+                    
+            }
+            
+        }); 
+
+
+          $('#iceberg').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'top': '460%'},500);
+
+                    
+            }
+            
+        }); 
+
+
+
+           $('#text2b').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '534%'},400);
+
+                    
+            }
+            
+        }); 
+
+         $('#front').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '603%'},400);
+
+                    
+            }
+            
+        }); 
+
+         $('#text2c').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '710%'},400);
+
+                    
+            }
+            
+        }); 
+
+         $('#outfit').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'top': '745%'},500);
+
+                    
+            }
+            
+        }); 
+
+         $('#subtitle2').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 200;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '835%'},400);
+
+                    
+            }
+            
+        }); 
+
+         $('#lego').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '916%'},400);
+
+                    
+            }
+            
+        }); 
+
+
+         $('#text3').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'top': '957%'},500);
+
+                    
+            }
+            
+        }); 
+
+         $('#souvenir').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '1027%'},400);
+
+                    
+            }
+            
+        }); 
+
+         $('#subtitle3').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 200;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '1142%'},400);
+
+                    
+            }
+            
+        }); 
+
+          $('#titanicsign').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '1215%'},400);
+
+                    
+            }
+            
+        }); 
+
+
+          $('#text3b').each( function(i){
+
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 500;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'top': '1315%'},400);
+
+                    
+            }
+            
+        }); 
+
+
+
+     });
+    
+});
