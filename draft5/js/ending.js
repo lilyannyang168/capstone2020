338254a31a9311d6b7ce43bf40fb6d552c26c77d
lilyannyang168 <36946555+lilyannyang168@users.noreply.github.com>
2020-04-20 04:51:26 -0500

    Add files via upload

--- draft5/js/ending.js
+++ draft5/js/ending.js
@@ -7,11 +7,6 @@ console.log('hi');
 $(document).ready(function() {
 
 	$('.dot1').mouseover('click touchstart',function(){
-    $(".dot1").animate({
-     
-      opacity: 0,
-
-    },0);
 
     $("#attraction").animate({
      
@@ -27,13 +22,65 @@ $(document).ready(function() {
 
 
     document.getElementById("attraction").innerHTML = "Gum Wall" ;
-    document.getElementById("city").innerHTML = "Seattle, Washington" ;
+    document.getElementById("city").innerHTML = "Seattle, WA" ;
 
 
   });
 
 	$('.dot1').mouseleave('click touchstart',function(){
     $(".dot1").animate({
+     
+      opacity: 0,
+
+    },250);
+
+    $("#attraction").animate({
+     
+      opacity: 0,
+
+    },250);
+
+    $("#city").animate({
+     
+      opacity: 0,
+
+    },250);
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+	$('.dot2').mouseover('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },250);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },250);
+
+
+    document.getElementById("attraction").innerHTML = "Madonna Inn" ;
+    document.getElementById("city").innerHTML = "San Luis Obispo, CA" ;
+
+
+  });
+
+	$('.dot2').mouseleave('click touchstart',function(){
+    $(".dot2").animate({
      
       opacity: 0,
 
