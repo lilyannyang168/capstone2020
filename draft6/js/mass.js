591aa0d6788fcfd6ca47b55940f9fe20815777b6
lilyannyang168 <36946555+lilyannyang168@users.noreply.github.com>
2020-04-22 04:22:32 -0500

    Add files via upload

---
+++ draft6/js/mass.js
@@ -0,0 +1,304 @@
+console.log('hi');
+
+$(document).ready(function() {
+
+	$('#lego').mouseover('click touchstart',function(){
+    $("#lego").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+	$('#lego').mouseover('click touchstart',function(){
+    $(".caption1").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#lego').mouseleave('click touchstart',function(){
+    $("#lego").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#lego').mouseleave('click touchstart',function(){
+    $(".caption1").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+  $('#souvenir').mouseover('click touchstart',function(){
+    $("#souvenir").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#souvenir').mouseover('click touchstart',function(){
+    $(".caption2").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#souvenir').mouseleave('click touchstart',function(){
+    $("#souvenir").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+
+
+
+  $('#place1').mouseleave('click touchstart',function(){
+    $(".caption3").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+  $('#place1').mouseover('click touchstart',function(){
+    $("#place1").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#place1').mouseover('click touchstart',function(){
+    $(".caption3").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#place1').mouseleave('click touchstart',function(){
+    $("#place1").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#place1').mouseleave('click touchstart',function(){
+    $(".caption3").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+   $('#locations').mouseover('click touchstart',function(){
+    $("#locations").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#locations').mouseover('click touchstart',function(){
+    $(".caption4").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#locations').mouseleave('click touchstart',function(){
+    $("#locations").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#locations').mouseleave('click touchstart',function(){
+    $(".caption4").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+   $('#america').mouseover('click touchstart',function(){
+    $("#america").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#america').mouseover('click touchstart',function(){
+    $(".caption5").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#america').mouseleave('click touchstart',function(){
+    $("#america").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#america').mouseleave('click touchstart',function(){
+    $(".caption5").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+  $('#bear').mouseover('click touchstart',function(){
+    $("#bear").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#bear').mouseover('click touchstart',function(){
+    $(".caption6").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#bear').mouseleave('click touchstart',function(){
+    $("#bear").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#bear').mouseleave('click touchstart',function(){
+    $(".caption6").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+
+
+    $('#locations2').mouseover('click touchstart',function(){
+    $("#locations2").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#locations2').mouseover('click touchstart',function(){
+    $(".caption6a").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#locations2').mouseleave('click touchstart',function(){
+    $("#locations2").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#locations2').mouseleave('click touchstart',function(){
+    $(".caption6a").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+
+    $('#bubbles').mouseover('click touchstart',function(){
+    $("#bubbles").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#bubbles').mouseover('click touchstart',function(){
+    $(".caption7").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#bubbles').mouseleave('click touchstart',function(){
+    $("#bubbles").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#bubbles').mouseleave('click touchstart',function(){
+    $(".caption7").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+
+  $('#nails').mouseover('click touchstart',function(){
+    $("#nails").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#nails').mouseover('click touchstart',function(){
+    $(".caption8").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#nails').mouseleave('click touchstart',function(){
+    $("#nails").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#nails').mouseleave('click touchstart',function(){
+    $(".caption8").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+
+
+
+
+
+
+});
