5ecf2eea3ec6ae9701daf721ffc0cc59f3469d27
lilyannyang168 <36946555+lilyannyang168@users.noreply.github.com>
2020-04-22 05:01:05 -0500

    Add files via upload

---
+++ draft6/js/bright.js
@@ -0,0 +1,183 @@
+console.log('hi');
+
+$(document).ready(function() {
+
+	$('#sign1').mouseover('click touchstart',function(){
+    $("#sign1").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+	$('#sign1').mouseover('click touchstart',function(){
+    $(".caption1").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#sign1').mouseleave('click touchstart',function(){
+    $("#sign1").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#sign1').mouseleave('click touchstart',function(){
+    $(".caption1").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+
+  $('#blake').mouseover('click touchstart',function(){
+    $("#blake").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#blake').mouseover('click touchstart',function(){
+    $(".caption2").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#blake').mouseleave('click touchstart',function(){
+    $("#sign1").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#blake').mouseleave('click touchstart',function(){
+    $(".caption2").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+   $('#hollywood').mouseover('click touchstart',function(){
+    $("#hollywood").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#hollywood').mouseover('click touchstart',function(){
+    $(".caption3").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#hollywood').mouseleave('click touchstart',function(){
+    $("#sign1").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#hollywood').mouseleave('click touchstart',function(){
+    $(".caption3").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+   $('#towels').mouseover('click touchstart',function(){
+    $("#towels").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#towels').mouseover('click touchstart',function(){
+    $(".caption4").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#towels').mouseleave('click touchstart',function(){
+    $("#sign1").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#towels').mouseleave('click touchstart',function(){
+    $(".caption4").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+
+   $('#cool').mouseover('click touchstart',function(){
+    $("#cool").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#cool').mouseover('click touchstart',function(){
+    $(".caption5").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#cool').mouseleave('click touchstart',function(){
+    $("#cool").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#cool').mouseleave('click touchstart',function(){
+    $(".caption5").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+});
