1d3cfc834e2ae6c9a6be663e332dfd15ec65ef68
lilyannyang168 <36946555+lilyannyang168@users.noreply.github.com>
2020-04-22 03:18:01 -0500

    Add files via upload

--- draft6/js/familiarity.js
+++ draft6/js/familiarity.js
@@ -136,5 +136,101 @@ $(document).ready(function() {
 
 
 
+      $('#rushmore').mouseover('click touchstart',function(){
+    $("#rushmore").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#rushmore').mouseover('click touchstart',function(){
+    $(".caption5").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#rushmore').mouseleave('click touchstart',function(){
+    $("#rushmore").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#rushmore').mouseleave('click touchstart',function(){
+    $(".caption5").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
+
+  $('#leo').mouseover('click touchstart',function(){
+    $("#leo").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#leo').mouseover('click touchstart',function(){
+    $(".caption6").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#leo').mouseleave('click touchstart',function(){
+    $("#leo").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#leo').mouseleave('click touchstart',function(){
+    $(".caption6").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+$('#gator').mouseover('click touchstart',function(){
+    $("#gator").animate({
+     
+      opacity: .2,
+
+    },500);
+  });
+
+  $('#gator').mouseover('click touchstart',function(){
+    $(".caption7").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#gator').mouseleave('click touchstart',function(){
+    $("#gator").animate({
+     
+      opacity: 1,
+
+    },500);
+  });
+  $('#gator').mouseleave('click touchstart',function(){
+    $(".caption7").animate({
+     
+      opacity: 0,
+
+    },500);
+  });
+
+
+
 
 });
