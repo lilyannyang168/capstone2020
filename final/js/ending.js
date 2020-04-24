83cbfad63a038e5eea49c998d79e8731b4511c6e
lilyannyang168 <36946555+lilyannyang168@users.noreply.github.com>
2020-04-24 04:20:58 -0500

    Add files via upload

--- final/js/ending.js
+++ final/js/ending.js
@@ -1,51 +1,25 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
 console.log('hi');
 
 $(document).ready(function() {
 
-	$('.dot1').mouseover('click touchstart',function(){
+$('#right').mouseover('click touchstart',function(){
 
-    $("#attraction").animate({
+    $(".rectangle").animate({
      
-      opacity: 1,
+      opacity: .8,
 
-    },250);
-
-    $("#city").animate({
-     
-      opacity: 1,
-
-    },250);
-
-
-    document.getElementById("attraction").innerHTML = "Gum Wall" ;
-    document.getElementById("city").innerHTML = "Seattle, WA" ;
-
+    },);
 
   });
 
-	$('.dot1').mouseleave('click touchstart',function(){
-    $(".dot1").animate({
+
+$('#right').mouseleave('click touchstart',function(){
+
+    $(".rectangle").animate({
      
       opacity: 0,
 
-    },250);
-
-    $("#attraction").animate({
-     
-      opacity: 0,
-
-    },250);
-
-    $("#city").animate({
-     
-      opacity: 0,
-
-    },250);
-
+    },);
 
   });
 
@@ -55,51 +29,6 @@ $(document).ready(function() {
 
 
 
-
-
-
-	$('.dot2').mouseover('click touchstart',function(){
-
-    $("#attraction").animate({
-     
-      opacity: 1,
-
-    },250);
-
-    $("#city").animate({
-     
-      opacity: 1,
-
-    },250);
-
-
-    document.getElementById("attraction").innerHTML = "Madonna Inn" ;
-    document.getElementById("city").innerHTML = "San Luis Obispo, CA" ;
-
-
-  });
-
-	$('.dot2').mouseleave('click touchstart',function(){
-    $(".dot2").animate({
-     
-      opacity: 0,
-
-    },250);
-
-    $("#attraction").animate({
-     
-      opacity: 0,
-
-    },250);
-
-    $("#city").animate({
-     
-      opacity: 0,
-
-    },250);
-
-
-  });
 
 
 });
