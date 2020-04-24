a6aa5ff5b124335d994606300a0fd3546ddcc426
lilyannyang168 <36946555+lilyannyang168@users.noreply.github.com>
2020-04-24 02:01:59 -0500

    Add files via upload

---
+++ final/js/endmap.js
@@ -0,0 +1,509 @@
+console.log('hi');
+
+$(document).ready(function() {
+
+  $('#seattle').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Gum Wall" ;
+    document.getElementById("city").innerHTML = "Seattle, WA" ;
+    document.getElementById("blurb").innerHTML = "For over 20 years, people have visited this old brick wall and covered it in chewing gum, making it a notable landmark near Pike Place Market" ;
+
+
+  });
+
+
+
+  $('#sanluis').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Madonna Inn" ;
+    document.getElementById("city").innerHTML = "San Luis Obispo, CA" ;
+    document.getElementById("blurb").innerHTML = "A pink resort that has a different theme for every room, with each as whimsical and ostentatious as the next." ;
+
+
+
+  });
+
+    $('#la').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Hollywood" ;
+    document.getElementById("city").innerHTML = "Los Angeles, CA" ;
+    document.getElementById("blurb").innerHTML = "It is known for its Walk of Fame with stars representing notable celebrities. The Hollywood sign is also frequently admired." ;
+
+
+  });
+
+
+     $('#calipatria').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Salvation Mountain" ;
+    document.getElementById("city").innerHTML = "Calipatria, CA" ;
+    document.getElementById("blurb").innerHTML = "A colorfully painted 150 feet wide by 50 feet tall hill that honors religion but also serves as a piece of artwork.  " ;
+
+
+  });
+
+
+
+
+     $('#vegas').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Las Vegas Strip" ;
+    document.getElementById("city").innerHTML = "Las Vegas, NV" ;
+    document.getElementById("blurb").innerHTML = "A 4.2 mile stretch of neon structures with many of them being replicas of other landmarks" ;
+
+
+  });
+
+      $('#santa').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Meow Wolf" ;
+    document.getElementById("city").innerHTML = "Santa Fe, NM" ;
+    document.getElementById("blurb").innerHTML = "Advertised as a psychedelic playground, it is a series of large-scale art installations set in a single house." ;
+
+
+  });
+
+
+
+      $('#bloom').click('click touchstart',function(){
+
+   $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Mall of America" ;
+    document.getElementById("city").innerHTML = "Bloomington, MN" ;
+    document.getElementById("blurb").innerHTML = "The biggest mall in America that has a Nickolodeon-themed amusement park in the middle of it." ;
+
+
+  });
+
+
+
+      $('#stl').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },250);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },250);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },250);
+
+
+    document.getElementById("attraction").innerHTML = "City Museum" ;
+    document.getElementById("city").innerHTML = "St. Louis, MO" ;
+    document.getElementById("blurb").innerHTML = "A playground for kids and adults alike that is made mostly from abandoned architectural and industrial objects" ;
+
+
+  });
+
+
+      $('#memphis').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Graceland" ;
+    document.getElementById("city").innerHTML = "Memphis, TN" ;
+    document.getElementById("blurb").innerHTML = "A mansion once owned by Elvis that contains many of his possessions and memorabilia." ;
+
+
+  });
+
+
+
+
+$('#pigeon').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Dollywood" ;
+    document.getElementById("city").innerHTML = "Pigeon Forge, TN" ;
+    document.getElementById("blurb").innerHTML = "Located right next to the Strip, it is an amusement park owned by Dolly Parton and pays homage to her and her faith." ;
+
+
+  });
+
+
+$('#atlanta').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "World of Coca-Cola" ;
+    document.getElementById("city").innerHTML = "Atlanta, GA" ;
+    document.getElementById("blurb").innerHTML = "A 20-acre museum that displays the history of the company based in Atlanta." ;
+
+
+  });
+
+
+$('#orlando').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "International Drive" ;
+    document.getElementById("city").innerHTML = "Orlando, FL" ;
+    document.getElementById("blurb").innerHTML = "A commercial area near Walt Disney World that contains attractions and shops similar to those found in Pigeon Forge." ;
+
+
+  });
+
+
+
+$('#niagra').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Niagra Falls" ;
+    document.getElementById("city").innerHTML = "Niagra Falls, NY" ;
+    document.getElementById("blurb").innerHTML = "Aside from being one of the most iconic natural landmarks, there are attractions such as Hershey's Chocolate World and Oh Cananda Eh? Dinner Theater. " ;
+
+
+  });
+
+
+$('#baltimore').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Papermoon Diner" ;
+    document.getElementById("city").innerHTML = "Baltimore, MD" ;
+    document.getElementById("blurb").innerHTML = "" ;
+
+
+  });
+
+
+$('#baltimore').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Papermoon Diner" ;
+    document.getElementById("city").innerHTML = "Baltimore, MD" ;
+    document.getElementById("blurb").innerHTML = "An over-decorated, brightly-colored diner that stands out in the neighborhood." ;
+
+
+  });
+
+$('#atlantic').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+
+    document.getElementById("attraction").innerHTML = "Atlantic City" ;
+    document.getElementById("city").innerHTML = "Atlantic City, NJ" ;
+    document.getElementById("blurb").innerHTML = "Its boardwalk has countless brightly-lit attractions such as a Ripley's Believe It or Not! Museum. " ;
+
+
+  });
+
+$('#ny').click('click touchstart',function(){
+
+    $("#attraction").animate({
+     
+      opacity: 1,
+
+    },200);
+
+    $("#city").animate({
+     
+      opacity: 1,
+
+    },400);
+
+    $("#blurb").animate({
+     
+      opacity: 1,
+
+    },600);
+
+    document.getElementById("attraction").innerHTML = "Times Square" ;
+    document.getElementById("city").innerHTML = "New York, NY" ;
+    document.getElementById("blurb").innerHTML = "An overwhelming conglomerate of consumerism and fun with a Madame Tussauds, Hershey's Chocolate World, and every other chain attraction imaginable. Its 55 giant LED displays never turn off. " ;
+
+
+  });
+
+
+
+
+
+
+
+
+
+});
